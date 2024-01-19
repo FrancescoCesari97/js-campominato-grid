00cc5b62f0e15a3af5ad1e6fad9aa6ff9a95b858
FrancescoCesari97 <freedus97@gmail.com>
2024-01-19 15:44:54 +0100

    aggiunta prova livelli e generatorwe bombe

--- main.js
+++ main.js
@@ -4,6 +4,10 @@
 
 const gameBoardElement = document.getElementById("gameboard");
 const playButonElement = document.getElementById("play-button");
+const levelSelectorElement = document.getElementById("level-selector");
+const easyLevelElement = document.getElementById("easy");
+const mediumLevelElement = document.getElementById("medium");
+const hardLevelElement = document.getElementById("hard");
 
 generateGrid(gameBoardElement);
 
@@ -15,19 +19,60 @@ function generateGrid(container) {
   // genero le celle  per 100 volte
   for (let i = 0; i < 100; i++) {
     // collego la funzione genera cella-
-    const cellElement = generateCell();
+    const cellElement = generateCell(i);
     container.append(cellElement);
   }
 }
 
+// * generazione 16 bombe
+
+// funzione generazione numero randomico
+
+let randomBombs = [];
+
+while (randomBombs.length < 16) {
+  const randomNumber = Math.floor(Math.random() * 100 + 1);
+  if (!randomBombs.includes(randomNumber)) {
+    randomBombs.push(randomNumber);
+  }
+}
+
+console.log(randomBombs);
+
+levelSelectorElement.addEventListener("select", function () {
+  if (levelSelectorElement == easyLevelElement) {
+    for (let i = 0; i < 100; i++) {
+      // collego la funzione genera cella-
+      const cellElement = generateCell(i);
+      container.append(cellElement);
+    }
+  } else if (levelSelectorElement == mediumLevelElement) {
+    for (let i = 0; i < 81; i++) {
+      // collego la funzione genera cella-
+      const cellElement = generateCell(i);
+      container.append(cellElement);
+    }
+  } else if (levelSelectorElement == hardLevelElement) {
+    for (let i = 0; i < 49; i++) {
+      // collego la funzione genera cella-
+      const cellElement = generateCell(i);
+      container.append(cellElement);
+    }
+  }
+});
+
 //- genrare con una funzione le celle dentro la griglia
 
-function generateCell() {
+function generateCell(text) {
   const cell = document.createElement("div");
   cell.classList.add("box");
   cell.addEventListener("click", function () {
-    handCellClick(this);
+    //handCellClick(this);
+    cell.innerText = text;
+
+    this.classList.toggle("clicked");
   });
+
   return cell;
 }
 
